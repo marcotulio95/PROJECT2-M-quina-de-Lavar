9a50c9ce8143aee25104dcefbc4a8dd7cb94398f
Marco Tulio <marco.tulio95@hotmail.com>
2017-03-22 02:20:43 -0300

    Difinicao dos Pinos de entrada dos perifericos

--- Projeto_2/Projeto_2.ino
+++ Projeto_2/Projeto_2.ino
@@ -1,19 +1,19 @@
 #include <EEPROM.h>
 
+#define bomba =  13; // Pino que ativa a bomba
+#define motor =  12; // Pino que ativa o motor
 
-// o endereço atual na EEPROM (isto é, é a posição do
-// byte que gravaremos dentro da EEPROM)
-int addr = 0;
+#define led_nivel_1 =  11; // Pino que o led1 (Nivel 1)
+#define led_nivel_2 =  10; //Pino que o led2 (Nivel 2)
+#define led_nivel_3 =  9; //Pino que o led3 (Nivel 3)
 
-typedef struct {
-    boolean motor_on_off; // 0 - Motor Desligado/1 - Motor Ligado
-    int tempo; // Tempo que o motor ficara ligado
-  }modo_rapido;
-  
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int bomba =  13;      // Pino que ativa a bomba
-const int motor =  12;      // Pino que ativa o motor
+#define led_modo_1 =  8; // Pino que o led1 (Nivel Leve)
+#define led_modo_2 =  7; //Pino que o led2 (Nivel Normal)
+#define led_modo_3 =  6; //Pino que o led3 (Nivel Pesado)
 
+#define button_seleciona_nivel = 5; // Pino para entrada do nivel desejado
+#define button_seleciona_modo = 4; // Pino para entrada do modo desejado
+int addr = 0; // Posicao Inicial da Memoria
 const int LM35 = A0; // Pino que verifica o nivel de agua
 float temperatura; // Variável que armazenará a temperatura medida
 int   saida;
@@ -25,9 +25,21 @@ void setup() {
   
   Serial.begin(9600); // inicializa a comunicação serial
   
-  // initialize the LED pin as an output:
+  // Inicialia os pinos de saida
   pinMode(bomba, OUTPUT);
   pinMode(motor, OUTPUT);
+    
+  pinMode(led_nivel_1, OUTPUT);
+  pinMode(led_nivel_2, OUTPUT);
+  pinMode(led_nivel_3, OUTPUT);
+  
+  pinMode(led_modo_1, OUTPUT);
+  pinMode(led_modo_2, OUTPUT);
+  pinMode(led_modo_3, OUTPUT);
+
+//Inicializa os pinos de entrada
+  pinMode(button_seleciona_nivel, INPUT);
+  pinMode(button_seleciona_modo, INPUT);
 }
 
 void lerEprom(){
@@ -114,11 +126,12 @@ void ligar_motor(int tempo){
     digitalWrite(motor, LOW);
   }
    
-void modoRapido(){
+boolean modoRapido(){
  
   // Ligar a bomba para colocar agua (Nivel);
   ligar_bomba('1'); // Passando nivel 1 de agua
   ligar_motor(5); // Passando 5 minutos
+  delay(1000); // Molho
   ligar_motor(5); // Passando 5 minutos 
   ligar_bomba('0');// Ligar a bomba para tirar agua; (Tirando agua suja)
   ligar_bomba('1');// Ligar a bomba para colcoar agua(Nivel); (Colocando agua limpa)
@@ -126,7 +139,42 @@ void modoRapido(){
   ligar_bomba('0');// Ligar a bomba para tirar agua; (Tirando agua enxague)
   ligar_motor(5); // Ligar o motor; (Centrifugando)
   return true;
+  
 } 
+boolean modoNormal(){
+    
+    ligar_bomba('1'); // Passando nivel 1 de agua
+    ligar_motor(10); // Ligar motor por 10 minutos
+    delay(5000); // Molho por 5 min
+    ligar_motor(10); // Ligar motor por 10 minutos
+    delay(5000); // Molho por 5 min
+    ligar_bomba('0');// Ligar a bomba para tirar agua; (Tirando agua suja)
+    ligar_bomba('1');// Ligar a bomba para colcoar agua(Nivel); (Colocando agua limpa)
+    ligar_motor(5); // Ligar motor por 5 minutos
+    ligar_bomba('0');// Ligar a bomba para tirar agua; (Tirando agua suja)
+    ligar_motor(10); // Ligar o motor; (Centrifugando)
+    return true;
+ }
+
+ boolean modoPesado(){
+    
+    ligar_bomba('1'); // Passando nivel 1 de agua
+    ligar_motor(15); // Ligar motor por 15 minutos
+    delay(10000); // Molho por 10 min
+    ligar_motor(15); // Ligar motor por 15 minutos
+    delay(10000); // Molho por 10 min
+    ligar_bomba('0');// Ligar a bomba para tirar agua; (Tirando agua suja)
+    ligar_bomba('1');// Ligar a bomba para colcoar agua(Nivel); (Colocando agua limpa)
+    ligar_motor(5); // Ligar motor por 5 minutos
+    ligar_bomba('0');// Ligar a bomba para tirar agua; (Tirando agua suja)
+    ligar_bomba('1');// Ligar a bomba para colcoar agua(Nivel); (Colocando agua limpa)
+    ligar_motor(5); // Ligar motor por 5 minutos
+    ligar_bomba('0');// Ligar a bomba para tirar agua; (Tirando agua suja)
+    ligar_motor(15); // Ligar o motor; (Centrifugando)
+    return true;
+ 
+ }
+
 
 void loop() {
   // read the state of the pushbutton value:
